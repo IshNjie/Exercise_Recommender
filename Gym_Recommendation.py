da3d092bd7fd2fce602779fcebf3acc56cae0a39
Ishmael N'jie <ishmaelnjie@ishmaels-mbp.home>
2022-04-06 22:12:29 +0100

    Updated README and Notebooks

--- Gym_Recommendation.py
+++ Gym_Recommendation.py
@@ -4,7 +4,6 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-from sklearn.metrics.pairwise import linear_kernel
 from sklearn.metrics.pairwise import cosine_similarity
 #https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 
@@ -12,19 +11,27 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 class reconSys():
     url = 'https://en.wikipedia.org/wiki/List_of_weight_training_exercises'
-    
+    #Set the url to retrieve the information
     
     def scrapePage(self):
         resp = requests.get(self.url)
         #Web scrapping library called beautifulsoup
         soup = BeautifulSoup(resp.text,'lxml')
+        
+        #Locate the table needed from the wiki page 
         table = soup.find_all('table','wikitable')[0]
-        df = pd.read_html(str(table))
-        df = pd.concat(df)
+
+        #Convert HTML text to df
+        df = pd.read_html(str(table))[0]
+        #df = pd.concat(df)
         return df
     
     def prep(self):
+
+        #Get df from scrape function
         df = self.scrapePage()
+
+        #Clean the data and replace text with binary values - treating N/A as 0
         df = df.fillna(0).replace(['Some','Yes'],1)
         df['Exercise'] = df['Exercise'].str.title()
         return df
@@ -32,23 +39,37 @@ class reconSys():
     
     def exerciseRecommend(self,exercise,no_exercises):
         df = self.prep()
+
+        #Get Index for each exercise
         indices = pd.Series(df.index, index = df['Exercise'])
+
+        #Create df to perform pairwise similarity
         df2 = df.iloc[:,df.columns != 'Exercise']
+
+        #Create matrix of similarity
         cosine_sim = cosine_similarity(df2,df2)
         
+        #Get Index for specified Exercise
         idx = indices[exercise]
+
+        #Get list of the cosine similarities of all exercises for specified exercise
         sim_scores = list(enumerate(cosine_sim[idx]))
+
+        #Filter list of tuples to exclude index for specified exercise, as that will be 1
         sim_scores = filter(lambda i:i[0] != idx, sim_scores)
+
+        #Sort tuples based on similarity measure, 2nd item in tuple
         sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
-    # Get the scores of the 5 most similar exercises
+        # Get the scores of the most similar exercises - filtered to amount specified
     
         sim_scores = sim_scores[0:no_exercises]
 
-    # Get the movie indices
+        # Get the list of exercies that are most similar
         movie_indices = [i[0] for i in sim_scores]
+
         print('Top {} Exercises based on {}'.format(no_exercises, exercise))
-    # Return the top most similar exercises
+        # Return the top most similar exercises by filtering the starting dataframe
         return df['Exercise'].iloc[movie_indices]
 
 
---
+++ README.md
@@ -0,0 +1,7 @@
+# Exercise Recommender
+
+Using Wikipedia to provide a breakdown of exercies and what muscle groups they target, an engine can be created to suggest similar exercises to ones provided
+
+Locate rationale around Recommendation Engines in the Notebooks folder
+
+There also has a Notebook of the code being run with outputs
